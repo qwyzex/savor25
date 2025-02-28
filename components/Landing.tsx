5abfa2d77e29d75415b8975b2a82a84fc467822f
qwyzex <qwyzex@gmail.com>
2025-02-28 16:21:57 +0700

    bf adding grain textures

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -5,19 +5,38 @@ const Landing = () => {
     return (
         <>
             <main className={styles.container}>
+                <div className={styles.gradientBlobContainer}>
+                    <svg xmlns="http://www.w3.org/2000/svg">
+                        <defs>
+                            <filter id="goo">
+                                <feGaussianBlur
+                                    in="SourceGraphic"
+                                    stdDeviation="10"
+                                    result="blur"
+                                />
+                                <feColorMatrix
+                                    in="blur"
+                                    mode="matrix"
+                                    values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -8"
+                                    result="goo"
+                                />
+                                <feBlend in="SourceGraphic" in2="goo" />
+                            </filter>
+                        </defs>
+                    </svg>
+                    <div className={styles.g1}></div>
+                    <div className={styles.g2}></div>
+                    <div className={styles.g3}></div>
+                    <div className={styles.g4}></div>
+                    <div className={styles.g5}></div>
+                    <div className="g6"></div>
+                </div>
                 <div className={styles.wrapper}>
-                    <div>
+                    <div className={styles.title}>
                         <h1>SAVOR</h1>
                         <p>2025</p>
                     </div>
                     <footer>
-                        {/* <Image
-                        src={"/imgs/sun_grain.png"}
-                        alt="asdf"
-                        width={200}
-                        height={200}
-                    /> */}
-
                         <div className={styles.orb}>
                             <div className={styles.shroud}></div>
                         </div>
