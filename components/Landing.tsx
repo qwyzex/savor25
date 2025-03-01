a26952a7e9f4456c11467190eb1aa8025ed98978
qwyzex <qwyzex@gmail.com>
2025-03-01 18:00:16 +0700

    First Github Initialization

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -38,7 +38,35 @@ const Landing = () => {
                     </div>
                     <footer>
                         <div className={styles.orb}>
-                            <div className={styles.shroud}></div>
+                            {/* <div className={styles.shroud}></div> */}
+                            <div className={styles.reflection}></div>
+                            <div className={styles.innerGlow}></div>
+                            <div className={styles.outerShadow}></div>
+                            {/* <div className={styles.orbGradientBlob}>
+                                <svg xmlns="http://www.w3.org/2000/svg">
+                                    <defs>
+                                        <filter id="igoo">
+                                            <feGaussianBlur
+                                                in="SourceGraphic"
+                                                stdDeviation="10"
+                                                result="blur"
+                                            />
+                                            <feColorMatrix
+                                                in="blur"
+                                                mode="matrix"
+                                                values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -8"
+                                                result="igoo"
+                                            />
+                                            <feBlend in="SourceGraphic" in2="igoo" />
+                                        </filter>
+                                    </defs>
+                                </svg>
+                                <div className={styles.g1}></div>
+                                <div className={styles.g2}></div>
+                                <div className={styles.g3}></div>
+                                <div className={styles.g4}></div>
+                                <div className={styles.g5}></div>
+                            </div> */}
                         </div>
                     </footer>
                 </div>
