3221507e41f7cbf1ee7ade8521f289b90b4eab32
ifa <qwyzex@gmail.com>
2025-04-15 14:02:28 +0700

    BACKUP BF PERSONA 5

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,11 +3,24 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
     return (
         <Html lang="en">
+            <div id="LOVE">
+                <ul>
+                    <li>
+                        <p>HOME</p>
+                    </li>
+                    <li>
+                        <p>REGISTER</p>
+                    </li>
+                    <li>
+                        <p>GUIDEBOOK</p>
+                    </li>
+                    <li>
+                        <p>OF US</p>
+                    </li>
+                </ul>
+            </div>
             <Head />
             <body id="bodyReal">
-                <div id="LOVE">
-                    <h1>NAVBAR MENU HAHAHAHAH</h1>
-                </div>
                 <Main />
                 <NextScript />
             </body>
